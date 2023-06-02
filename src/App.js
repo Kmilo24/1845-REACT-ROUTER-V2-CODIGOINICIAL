7a607c7160cc220f44bd722ef8f896cff4adf851
Milolex <kmilomartinezkam@gmail.com>
2023-06-02 16:43:12 -0500

    Route Utilizando librerias

--- src/App.js
+++ src/App.js
@@ -2,25 +2,17 @@ import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
+
+import { Route, BrowserRouter as Router,Routes} from 'react-router-dom';
 function App() {
-  const location = window.location.pathname;
-
-
-  const Routes = () => {
-    if (location === '/') {
-      return <Home />
-    }else{
-      return <Sobre />
-    }
-  }
-  
+  const location = window.location.pathname;  
   return (
-    
-
-
-    <>
-      {Routes()}
-    </>
+    <Router>
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sobre' element={<Sobre/>}/>
+      </Routes>
+    </Router>
   );
 }
 
